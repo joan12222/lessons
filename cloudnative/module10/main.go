eecd90ad26cad7bf0442f0b9af903f51815941ad
joan12222 <joan1222@hotmail.com>
2022-04-25 00:30:10 +0800

    Add Makefile

--- cloudnative/module10/main.go
+++ cloudnative/module10/main.go
@@ -1,5 +1,91 @@
 package main
 
+import (
+	"fmt"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"log"
+	"math/rand"
+	"net/http"
+	"time"
+)
+
+func Register() {
+	err := prometheus.Register(functionLatency)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+const (
+	MetricsNamespace = "cloudnative"
+)
+
+// NewExecutionTimer provides a timer for Updater's RunOnce execution
+func NewTimer() *ExecutionTimer {
+	return NewExecutionTimer(functionLatency)
+}
+
+var (
+	functionLatency = CreateExecutionTimeMetric(MetricsNamespace,
+		"Time spent.")
+)
+
+// NewExecutionTimer provides a timer for admission latency; call ObserveXXX() on it to measure
+func NewExecutionTimer(histo *prometheus.HistogramVec) *ExecutionTimer {
+	now := time.Now()
+	return &ExecutionTimer{
+		histo: histo,
+		start: now,
+		last:  now,
+	}
+}
+
+// ObserveTotal measures the execution time from the creation of the ExecutionTimer
+func (t *ExecutionTimer) ObserveTotal() {
+	(*t.histo).WithLabelValues("total").Observe(time.Now().Sub(t.start).Seconds())
+}
+
+// CreateExecutionTimeMetric prepares a new histogram labeled with execution step
+func CreateExecutionTimeMetric(namespace string, help string) *prometheus.HistogramVec {
+	return prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Namespace: namespace,
+			Name:      "execution_latency_seconds",
+			Help:      help,
+			Buckets:   prometheus.ExponentialBuckets(0.001, 2, 15),
+		}, []string{"step"},
+	)
+}
+
+// ExecutionTimer measures execution time of a computation, split into major steps
+// usual usage pattern is: timer := NewExecutionTimer(...) ; compute ; timer.ObserveStep() ; ... ; timer.ObserveTotal()
+type ExecutionTimer struct {
+	histo *prometheus.HistogramVec
+	start time.Time
+	last  time.Time
+}
+
+func images(w http.ResponseWriter, r *http.Request) {
+	timer := NewTimer()
+	defer timer.ObserveTotal()
+	randInt := rand.Intn(2000)
+	time.Sleep(time.Millisecond * time.Duration(randInt))
+	w.Write([]byte(fmt.Sprintf("<h1>%d<h1>", randInt)))
+}
+
+func healthz(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(fmt.Sprintf("<h1>timestamp: %d<h1>", time.Now())))
+}
+
 func main() {
-	metrics.Reg
+	Register()
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/images", images)
+	mux.Handle("/metrics", promhttp.Handler())
+	mux.HandleFunc("/healthz", healthz)
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatalf("start http server failed, error: %s\n", err.Error())
+	}
 }
